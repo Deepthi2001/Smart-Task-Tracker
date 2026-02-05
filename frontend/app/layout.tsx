ee8e849e7951a2f88b3b28a25294c83aabacb349
Deepthi2001 <saideepthi2001@gmail.com>
2026-02-05 09:46:46 -0500

    minor header css change

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body style={{ fontFamily: 'ui-sans-serif, system-ui', margin: 0, padding: 16 }}>
-        <h1>Smart Task Tracker</h1>
+        <h1 style={{ fontSize: '40px', fontWeight: 'bold', marginLeft: 60 }}>Smart Task Tracker</h1>
         {children}
       </body>
     </html>
